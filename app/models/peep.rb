544c33ce6a285cc220daf2474d92fd182fde9fac
csanann <csanann@gmail.com>
2023-05-12 12:15:27 +0100

    Defines initialize method

--- app/models/peep.rb
+++ app/models/peep.rb
@@ -2,4 +2,11 @@
 
 class Peep 
   attr_accessor :id, :message, :timestamp,:user_id
+
+  def initialize(id: nil, message:,timestamp:, user_id:)
+    @id = id
+    @message = message
+    @timestamp = timestamp
+    @user_id = user_id
+  end
 end
