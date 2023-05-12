70126203ce0abb4702ccd14aa4b2792da8723470
csanann <csanann@gmail.com>
2023-05-12 12:16:38 +0100

    Updates the file

--- app/models/peep_repository.rb
+++ app/models/peep_repository.rb
@@ -1,6 +1,8 @@
 #file: app/models/peep_repository.rb
+require_relative '../models/peep'
 
 
+=begin
 class PeepRepository
   def all
     peeps = []
@@ -42,5 +44,44 @@ class PeepRepository
     peep.user_id = record['user_id'].to_i
     
     return peep
+  end
+end
+=end
+class PeepRepository
+
+  def initialize(database_connection)
+    @database_connection = database_connection
+  end
+
+
+  def create(peep)
+    sql = 'INSERT INTO peeps (message, timestamp, user_id) VALUES ($1, $2, $3);'
+    params = [peep.message, peep.timestamp, peep.user_id]
+    @database_connection.exec_params(sql, params)
+    return nil
+  end  
+
+
+  def find_by_id(id)
+    sql = 'SELECT * FROM peeps WHERE id = $1;'
+    params = [id]
+    result = @database_connection.exec_params(sql, params)
+    return nil if result.ntuples.zero?
+    return create_peep(result[0])
+  end
+
+
+  def delete_by_id(id)
+    sql = 'DELETE FROM peeps WHERE id = $1;'
+    params = [id]
+    @database_connection.exec_params(sql, params)
+  end
+
+
+  private
+
+
+  def create_peep(record)
+    Peep.new(id: record['id'].to_i, message: record['message'], timestamp: record['timestamp'], user_id: record['user_id'].to_i)
   end
 end
